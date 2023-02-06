4aeb2111202e93c21b63bbf3f5eb29a0baca0370
PASSER-BY <zhenghaole1990@126.com>
2023-02-06 23:24:28 +0800

    updated 添加项目推荐脚本

---
+++ public/script/projects.js
@@ -0,0 +1,58 @@
+(function(){
+	var list = [{
+		'url':'https://jquerywidget.com/',
+		'name':'jQuery插件库'
+	},{
+		'url':'https://passer-by.com/pacman/',
+		'name':'吃豆人游戏'
+	},{
+		'url':'https://passer-by.com/data_location/',
+		'name':'行政区划数据'
+	},{
+		'url':'https://passer-by.com/data_post/',
+		'name':'邮政编码数据'
+	},{
+		'url':'https://passer-by.com/calendar/',
+		'name':'万年历'
+	},{
+		'url':'https://passer-by.com/datex/',
+		'name':'时间格式化'
+	},{
+		'url':'https://passer-by.com/browser/',
+		'name':'浏览器判断'
+	},{
+		'url':'https://passer-by.com/imageCode/',
+		'name':'imageCode脚本加密'
+	},{
+		'url':'https://passer-by.com/hiddenCode/',
+		'name':'hiddenCode脚本加密'
+	}];
+	var url = location['hostname']+location['pathname'];
+	document.write(`
+		<div class="mod-projects">
+			<div class="hd">
+				<a href="https://passer-by.com/project/" target="_blank">😉 更多开源项目</a>
+			</div>
+			<div class="bd">
+				<ul>
+					`+(function(){
+						return list.filter(function(item){
+							return item['url'].indexOf(url)==-1;
+						}).map(function(item){
+							return `<li><a href="${item['url']}" target="_blank">${item['name']}</a></li>`;
+						}).join('');
+					})()+`
+				</ul>
+			</div>
+		</div>
+		<style type="text/css">
+			.mod-projects{max-width:960px;margin:0 auto;padding: 30px 0;text-align:center;}
+			.mod-projects a{text-decoration:none;}
+			.mod-projects .hd{line-height:40px;font-size:18px;font-weight:bold;}
+			.mod-projects ul{overflow:hidden;}
+			.mod-projects li{float:left;width:25%;}
+			.mod-projects li a{display:block;margin: 0 1px 2px;line-height:36px;background: #f8f8f8;}
+			.mod-projects li a:hover{background: #e9f4ff}
+		</style>
+	`);
+})();
