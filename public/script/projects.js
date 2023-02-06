c536ac82f1b1a27b518fc10934c4d2fd8e2b4d6f
PASSER-BY <zhenghaole1990@126.com>
2023-02-07 00:03:12 +0800

    Update projects.js

--- public/script/projects.js
+++ public/script/projects.js
@@ -47,11 +47,11 @@
 		</div>
 		<style type="text/css">
 			.mod-projects{max-width:960px;margin:0 auto;padding: 30px 0 45px;text-align:center;}
-			.mod-projects a{text-decoration:none;color:#333;}
+			.mod-projects a{text-decoration:none;color:##6e7781;}
 			.mod-projects .hd{line-height:40px;font-size:18px;font-weight:bold;}
 			.mod-projects ul{overflow:hidden;list-style: none;}
 			.mod-projects li{float:left;width:25%;}
-			.mod-projects li a{display:block;margin: 0 1px 2px;line-height:36px;background: #f8f8f8;}
+			.mod-projects li a{display:block;margin: 0 1px 2px;line-height:36px;background: #f8f8f8;color:#6e7781;}
 			.mod-projects li a:hover{background: #e9f4ff}
 			@media screen and (max-width: 800px){
 				.mod-projects li{width:50%;}
