9812a6780256bb760515d38247832c87c69aa4ce
PASSER-BY <zhenghaole1990@126.com>
2023-05-12 01:30:39 +0800

    updated 样式调整

---
+++ public/script/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2be846d23757ef42d85e2d41ab0ef51b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
