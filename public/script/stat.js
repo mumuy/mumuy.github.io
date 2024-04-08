f571b7d370c7205aefc29373fee5cb7e43fd0cc1
路人甲 <zhenghaole1990@126.com>
2024-04-08 17:52:37 +0800

    Update stat.js

--- public/script/stat.js
+++ public/script/stat.js
@@ -1,7 +1,9 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2be846d23757ef42d85e2d41ab0ef51b";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?f9a2ca3a78be611722ecd5a78a0646c2";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
