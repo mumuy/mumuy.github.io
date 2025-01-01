37487ad05f3cdad662f99ed7f8dae6d1ccde0de6
mumuy <>
2025-01-01 19:32:49 +0800

    Update stat.js

--- public/script/stat.js
+++ public/script/stat.js
@@ -3,11 +3,9 @@ if(location.protocol.indexOf('http')>-1&&location.hostname.indexOf('passer-by.co
         location.href = 'https://passer-by.com/project.html';
     },parseInt(3000+5000*Math.random()));
 }else{
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?f9a2ca3a78be611722ecd5a78a0646c2";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "pn2m8urb4c");
 }
