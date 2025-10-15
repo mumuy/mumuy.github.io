1d78e3a55c579edf1ff8d00019767dccdf8d6e14
mumuy <zhenghaole1990@126.com>
2025-10-15 21:48:14 +0800

    Update projects.js

--- static/script/projects.js
+++ static/script/projects.js
@@ -74,7 +74,7 @@
 			.mod-projects{max-width:1000px;margin:0 auto 20px;padding: 30px 0;text-align:center;font-size:14px;}
 			.mod-projects a{text-decoration:none;color:#6e7781;}
 			.mod-projects .hd{line-height:40px;font-size:18px;font-weight:bold;}
-			.mod-projects ul{display:grid;grid-template-columns:repeat(4,1fr);gap:2;list-style: none;margin:0;padding:0;}
+			.mod-projects ul{display:grid;grid-template-columns:repeat(4,1fr);gap:2px;list-style: none;margin:0;padding:0;}
 			.mod-projects li a{display:block;line-height:36px;background: rgba(0,0,0,0.03);border-radius:3px;color:#6e7781;}
 			.mod-projects li a:hover{background: #e9f4ff}
 			.mod-spread{max-width:960px;margin:0 auto 20px;padding: 30px 0;text-align:center;}
