902f770e3884c4fd965ac9135f886734cc7ed803
mumuy <zhenghaole1990@126.com>
2025-10-31 11:36:50 +0800

    updated 广告代码统一

--- static/script/stat.js
+++ static/script/stat.js
@@ -3,5 +3,15 @@ if(location.protocol.indexOf('http')>-1&&location.hostname.indexOf('passer-by.co
         location.href = 'https://passer-by.com/project.html';
     },parseInt(3000+5000*Math.random()));
 }else{
+    // 统计
     !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3Ko4mboPiOG9ggEe",ck:"3Ko4mboPiOG9ggEe"});
+    // 广告
+    const $script = document.createElement('script');
+    $script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5322941251486103";
+    $script.async = true;
+    $script.crossOrigin = "anonymous";
+    const $head = document.head || document.getElementsByTagName('head')[0];
+    if ($head) {
+        $head.appendChild($script);
+    }
 }
